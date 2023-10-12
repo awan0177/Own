7efe0878246a8d066ae26842499950801f223702
awan0177 <147627523+awan0177@users.noreply.github.com>
2023-10-13 00:15:26 +0500

    Update Ak.py

--- Ak.py
+++ Ak.py
@@ -1317,7 +1317,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/awan0177/Awn.txt/blob/main/Approvel.txt").text
+	r1=requests.get("https://github.com/awan0177/Awn.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
